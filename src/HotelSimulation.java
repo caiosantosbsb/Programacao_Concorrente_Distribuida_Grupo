cedabfef4e507ae2fbdc5d21ccceb2db662f3e43
caiosantosbsb <caiosantosbsb@gmail.com>
2024-05-02 16:08:46 -0300

    validação classe quarto

--- src/HotelSimulation.java
+++ src/HotelSimulation.java
@@ -1,11 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class HotelSimulation {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Instanciar variaveis
+        int numQuartos = 10;
+        
+        //Instanciar quartos
+        //Quartos incialmente limpos, vazios e numerados
+        List<Quarto> quartos = new ArrayList<>();
+        for (int i = 1; i <= numQuartos; i++) {
+            quartos.add(new Quarto(i));
+        }
+
+        //Ver quartos criados
+        visualizarQuartos(quartos);
     }
     
+    private static void visualizarQuartos(List<Quarto> quartos) {
+        for (Quarto quarto : quartos) {
+            System.out.println("Quarto " + quarto.getNumero() +
+                               ": Ocupado? " + !quarto.isOcupado() +   // Note que invertemos a lógica baseado na sua implementação original
+                               ", Limpo? " + !quarto.isLimpo() +       // Idem
+                               ", Chave na Recepção? " + quarto.isChaveNaRecepcao());
+        }
+    }
 }
